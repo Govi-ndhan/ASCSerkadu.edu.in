dcdbea3f190e2439c80e2b2b16f183fcdda737a0
Govindhan A <144588962+Govi-ndhan@users.noreply.github.com>
2025-04-21 15:05:54 +0530

    Add files via upload

---
+++ assests/scrimgs/gallery/gallery.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Art&apos;s &amp; Science College, Serkadu</title>
+    <link rel="icon" type="image/x-icon" href="../college_logo.png">
+    <link rel="stylesheet" href="../../../styles/gallery.css">
+    <link rel="stylesheet" href="../../../styles/main.css">
+    <link rel="stylesheet" href="../../../styles/responsive.css">
+    <script src="../../../script/index.js"></script>
+    
+</head>
+<body>
+    <nav id="mainNav">
+        <div class="logo">Governtment Art's &amp; Science College, Serkadu</div>
+        <button class="mobile-menu" onclick="toggleMenu()">☰</button>
+        <ul class="nav-links">
+            <li><a href="javascript:void(0)" onclick ="location.href = '../../../index.html'">Home</a></li>
+            <li><a href="javascript:void(0)" onclick ="location.href = '../../pages/courses.html'">Courses</a></li>
+            <li><a href="javascript:void(0)" onclick ="location.href = '../../pages/faculty.html'">Faculty</a></li>
+            <li><a href="javascript:void(0)" onclick ="location.href = 'gallery.php'">Gallery</a></li>
+            <li><a href="javascript:void(0)" onclick ="location.href = '../../pages/document.html'">Login</a></li>
+            <li><a href="javascript:void(0)" onclick ="location.href = '../../pages/contact.html'">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section id="gallery" class="page">
+            <div class="content">
+                <h2>Gallery</h2>
+                <div class="sub-content">
+                        <?php
+                            $a = scandir(getcwd());
+                            foreach($a as $i){
+                                if($i=='.'||$i=='..'){
+                                    continue;
+                                }
+                                else{
+                                    if(str_ends_with($i,"jpg")){
+                                        echo "<a target='_blank' href ='".$i."'><img src = '".$i."' width = '10%' alt = 'img'></a>";
+                                    }
+                                }
+                            }
+                        ?>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 Governtment Art's &amp; Science College, Serkadu. All rights reserved.</p>
+        <p class="small-size">Powered By Governtment Art&apos;s &amp; Science College Students, Serkadu.</p>
+    </footer>
+
+</body>
+</html>
